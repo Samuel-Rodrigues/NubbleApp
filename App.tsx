154bb44fec6709e36efdb952f2f230dbab919c3a
Samuel Rodrigues <carlossamuel.rodrigues@gmail.com>
2023-10-06 17:59:57 -0300

    adicionado componente texto

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {Text} from './src/components/Text/Text';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Carlos Samuel Rodrigues</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
